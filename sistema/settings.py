456c766fd6734caf3a45b6868a1cbac6d875413b
Eduar01 <114963479+Eduar01@users.noreply.github.com>
2022-11-14 09:02:05 -0500

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-+!8kxy9a($_bm!aj!+y0b-q-s+$7nc!w0c2#0@c1714a_oww_w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['mascotas-xd.herokuapp.com', '127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['asociacionusar.herokuapp.com', '*']
+#ALLOWED_HOSTS = ['*']
 
  
 # Application definition
